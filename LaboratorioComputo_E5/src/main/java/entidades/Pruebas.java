da00dfcd0ca6b1d408332c742d960eca66d8e0e1
SebasGlez03 <sebas.glez18040465@gmail.com>
2024-10-21 22:38:17 -0700

    se eliminaron clases para verificar funcionamiento de BD
    Se eliminaron las siguientes clases:
    -- CentroComputoEntidad
    -- ComputadoraEntidad
    
    Se agrego la siguiente clase para hacer pruebas de BD:
    -- Pruebas

---
+++ LaboratorioComputo_E5/src/main/java/entidades/Pruebas.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author PC
+ */
+public class Pruebas {
+    public static void main(String[] args) {
+        // CREAMOS UNA FACTORY DE ENTITY MANAGERS
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("ConexionJPA");
+        // CREAMOS UN OBJETO EM QUE REPRESENTA A LA BD EN CÓDIGO
+        EntityManager entityManager = managerFactory.createEntityManager();
+        //INICIAMOS LA TRANSACCION
+        entityManager.getTransaction().begin();
+
+        UnidadAcademicaEntidad uaEntidad = new UnidadAcademicaEntidad("Nainari");
+
+        entityManager.persist(uaEntidad);
+
+        //MANDAMOS A EJECUTAR LA TRANSACCION
+        entityManager.getTransaction().commit();
+        //CERRAMOS
+        entityManager.close();
+        managerFactory.close();
+
+    }
+}
---
+++ LaboratorioComputo_E5/src/main/java/entidades/UnidadAcademicaEntidad.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author PC
+ */
+@Entity
+@Table(name = "tblUnidadAcademica")
+public class UnidadAcademicaEntidad implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUnidadAcademica")
+    private Long id;
+
+    @Column(name = "nombre", length = 50, nullable = false)
+    private String nombre;
+
+    /**
+     * Constructor por omision
+     */
+    public UnidadAcademicaEntidad() {
+    }
+
+    /**
+     * Constructor que inicializa el atributo de la clase "nombre"
+     *
+     * @param nombre nombre a agregar
+     */
+    public UnidadAcademicaEntidad(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Metodo que obtiene el id de la Unidad Academica
+     *
+     * @return id de la Unidad Academica a obtener
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Metodo que establece el id de la Unidad Academica
+     *
+     * @param id id de la Unidad Academica a establecer
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Metodo que obtiene el nombre de la Unidad Academica
+     *
+     * @return nombre de la Unidad Academica a obtener
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Metodo que establece el nombre de la Unidad Academica
+     *
+     * @param nombre nombre de la Unidad Academica a establecer
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UnidadAcademicaEntidad)) {
+            return false;
+        }
+        UnidadAcademicaEntidad other = (UnidadAcademicaEntidad) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.unidadAcademicaEntidad[ id=" + id + " ]";
+    }
+
+}
