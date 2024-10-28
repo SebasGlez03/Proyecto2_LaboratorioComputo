e9cb630678df9b042ca9a195887c1a85bdabf356
vvcrxyz <nomar.limon04@gmail.com>
2024-10-27 18:59:07 -0700

    Ya se agregan Carreras

--- LaboratorioComputo_E5/src/main/java/presentacion/AdminMenu/GestionarCarrera/FrmAgregarCarrera.java
+++ LaboratorioComputo_E5/src/main/java/presentacion/AdminMenu/GestionarCarrera/FrmAgregarCarrera.java
@@ -4,6 +4,14 @@
  */
 package presentacion.AdminMenu.GestionarCarrera;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import persistencia.DAO.CarreraDAO;
+import persistencia.entidades.CarreraEntidad;
+
 /**
  *
  * @author nomar
@@ -33,7 +41,7 @@ public class FrmAgregarCarrera extends javax.swing.JFrame {
         nombreCarrera = new javax.swing.JLabel();
         campoTextoNombreCarrera = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        campoTextoTiempo = new javax.swing.JTextField();
+        campoTextoTiempoDiario = new javax.swing.JTextField();
         fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -78,14 +86,15 @@ public class FrmAgregarCarrera extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Tiempo en minutos");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 190, -1, -1));
-        getContentPane().add(campoTextoTiempo, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 220, 230, 30));
+        jLabel1.setText("Tiempo Diario HH:MM:SS");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 190, 220, -1));
+        getContentPane().add(campoTextoTiempoDiario, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 220, 230, 30));
 
         fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/backgroundChico.jpg"))); // NOI18N
         getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
@@ -95,10 +104,34 @@ public class FrmAgregarCarrera extends javax.swing.JFrame {
 
     private void btnReiniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReiniciarActionPerformed
         // TODO add your handling code here:
+        campoTextoNombreCarrera.setText("");
+        campoTextoTiempoDiario.setText("");
     }//GEN-LAST:event_btnReiniciarActionPerformed
 
     private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
         // TODO add your handling code here:
+        
+        String nombreCarrera = campoTextoNombreCarrera.getText();
+    String tiempoStr = campoTextoTiempoDiario.getText();
+    
+    try {
+        // Convertir el tiempo de String a LocalTime y luego a Date
+        LocalTime tiempoDiarioLocal = LocalTime.parse(tiempoStr);
+        Date tiempoDiarioDate = Date.from(tiempoDiarioLocal.atDate(LocalDate.now())
+                .atZone(ZoneId.systemDefault())
+                .toInstant());
+        
+        // Crear instancia de CarreraEntidad con Date
+        CarreraEntidad carrera = new CarreraEntidad(nombreCarrera, tiempoDiarioDate, null);
+        
+        // Guardar en la base de datos
+        CarreraDAO carreraDAO = new CarreraDAO();
+        carreraDAO.guardarCarrera(carrera);
+        
+        JOptionPane.showMessageDialog(this, "Carrera agregada con éxito.");
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(this, "Error al guardar: " + e.getMessage());
+    }
     }//GEN-LAST:event_btnAgregarActionPerformed
 
     /**
@@ -142,7 +175,7 @@ public class FrmAgregarCarrera extends javax.swing.JFrame {
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnReiniciar;
     private javax.swing.JTextField campoTextoNombreCarrera;
-    private javax.swing.JTextField campoTextoTiempo;
+    private javax.swing.JTextField campoTextoTiempoDiario;
     private javax.swing.JLabel fondo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel nombreCarrera;
