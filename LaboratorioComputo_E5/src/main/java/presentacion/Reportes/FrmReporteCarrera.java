f9163fee027505e3a719c9ceeaeeac7845b4752d
SebasGlez03 <sebas.glez18040465@gmail.com>
2024-10-28 00:17:16 -0700

    se agrego dependencia itextpdf

--- LaboratorioComputo_E5/src/main/java/presentacion/Reportes/FrmReporteCarrera.java
+++ LaboratorioComputo_E5/src/main/java/presentacion/Reportes/FrmReporteCarrera.java
@@ -4,6 +4,11 @@
  */
 package presentacion.Reportes;
 
+import com.github.lgooddatepicker.components.DateTimePicker;
+import java.awt.FlowLayout;
+import java.util.List;
+import negocio.DTO.CarreraDTO;
+import negocio.logica.CarreraNegocio;
 import presentacion.AdminMenu.FrmReportes;
 
 /**
@@ -12,11 +17,39 @@ import presentacion.AdminMenu.FrmReportes;
  */
 public class FrmReporteCarrera extends javax.swing.JFrame {
 
+    CarreraNegocio carreraNegcio = new CarreraNegocio();
+    DateTimePicker dateTimePicker = new DateTimePicker();
+    DateTimePicker dateTimePicker2 = new DateTimePicker();
+
     /**
      * Creates new form FrmReporteCarrera
      */
     public FrmReporteCarrera() {
         initComponents();
+
+        fldFechaInicio.setLayout(new FlowLayout());
+        fldFechaInicio.add(dateTimePicker);
+        fldFechaFin.setLayout(new FlowLayout());
+        fldFechaFin.add(dateTimePicker2);
+        llenarBoxCarreras(carreraNegcio.buscarCarreras());
+    }
+
+    /**
+     * Metodo que se encarga de llenar el comboBox con las carreras de la base
+     * de datos
+     *
+     * @param carrera Lista de carreras que hay en una unidad academica
+     */
+    private void llenarBoxCarreras(List<CarreraDTO> carrera) {
+        int i = 0;
+        while (carrera.size() > i) {
+            comboBoxCarrera.addItem(carrera.get(i).getNombre());
+            i++;
+        }
+    }
+    
+    public void generarReporteCarrera(){
+        
     }
 
     /**
@@ -28,6 +61,12 @@ public class FrmReporteCarrera extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lblCarreras = new javax.swing.JLabel();
+        lblFechaFin = new javax.swing.JLabel();
+        lblFechaInicio = new javax.swing.JLabel();
+        comboBoxCarrera = new javax.swing.JComboBox<>();
+        fldFechaFin = new javax.swing.JPanel();
+        fldFechaInicio = new javax.swing.JPanel();
         reporte = new javax.swing.JLabel();
         carrera = new javax.swing.JLabel();
         btnAtras = new javax.swing.JLabel();
@@ -36,6 +75,25 @@ public class FrmReporteCarrera extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblCarreras.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblCarreras.setForeground(new java.awt.Color(255, 255, 255));
+        lblCarreras.setText("Carreras");
+        getContentPane().add(lblCarreras, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 440, -1, -1));
+
+        lblFechaFin.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblFechaFin.setForeground(new java.awt.Color(255, 255, 255));
+        lblFechaFin.setText("Fecha fin");
+        getContentPane().add(lblFechaFin, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 260, -1, -1));
+
+        lblFechaInicio.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblFechaInicio.setForeground(new java.awt.Color(255, 255, 255));
+        lblFechaInicio.setText("Fecha Inicio");
+        getContentPane().add(lblFechaInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 260, -1, -1));
+
+        getContentPane().add(comboBoxCarrera, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 480, 210, 40));
+        getContentPane().add(fldFechaFin, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 300, 350, 40));
+        getContentPane().add(fldFechaInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 300, 350, 40));
 
         reporte.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
         reporte.setForeground(new java.awt.Color(255, 255, 255));
@@ -107,7 +165,13 @@ public class FrmReporteCarrera extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel btnAtras;
     private javax.swing.JLabel carrera;
+    private javax.swing.JComboBox<String> comboBoxCarrera;
+    private javax.swing.JPanel fldFechaFin;
+    private javax.swing.JPanel fldFechaInicio;
     private javax.swing.JLabel fondo;
+    private javax.swing.JLabel lblCarreras;
+    private javax.swing.JLabel lblFechaFin;
+    private javax.swing.JLabel lblFechaInicio;
     private javax.swing.JLabel reporte;
     // End of variables declaration//GEN-END:variables
 }
